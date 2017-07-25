e33b9c10b58c7b2cc7ce3206895d0de8df1cdf50
Fuzen-py <fuzen@protonmail.com>
2017-07-25 16:26:55 -0400

    Fixed clock not using proper hex

--- clock.js
+++ clock.js
@@ -1,37 +1,49 @@
+var showhex = false;
+function to_hex(char) {
+	char = char.toString(16);
+	if (char.length > 1) {
+		return char;
+	}
+	return "0" + char;
+
+}
 function get_hex() {
 
 	var d = new Date(), h = d.getHours(), m = d.getMinutes(), s = d.getSeconds();
 
-	if (h <= 9)  h  = "0" + h;
-	if (m <= 9)  m  = "0" + m;
-	if (s <= 9)  s  = "0" + s;
-
+	h = to_hex(h);
+	m = to_hex(m);
+	s = to_hex(s);
 	return "#"+h+m+s;
 }
 
 function color_clock() {
 	var d = new Date(), h = d.getHours(), m = d.getMinutes(), s = d.getSeconds(), ms = d.getMilliseconds();
-
 	if (h <= 9)  h  = "0" + h;
 	if (m <= 9)  m  = "0" + m;
 	if (s <= 9)  s  = "0" + s;
 	if (ms <= 9) {ms = "00" + ms} else if (ms <= 99) {ms="0" +ms};
 
-
-	var color = "#"+h+m+s;
-	var current_time = h+":"+m+":"+s;
+	var color = get_hex();
+	var current_time = ""
+	if (showhex) {current_time = get_hex()} else {
+		current_time = h+":"+m+":"+s;
+	}
 	document.body.style.background = color;
-	document.getElementById("h").innerHTML = h;
-	document.getElementById("m").innerHTML = m;
-	document.getElementById("s").innerHTML = s;
-	document.getElementById("ms").innerHTML = ms;
+	document.getElementById("base-time").innerHTML = current_time;
+	if (showhex) {
+		document.getElementById("ms").innerHTML = "";
+		document.getElementById("ms-sep").innerHTML = "";
+	} else {
+		document.getElementById("ms").innerHTML = ms;
+		document.getElementById("ms-sep").innerHTML = ".";
+	}
 	document.getElementById('progress-bar').style.width = s/60*22 + '%';
 	setTimeout(function() {
 		color_clock();
 	}, 1);
 
 }
-
 color_clock();
 document.body.addEventListener('copy', function(e){
 	e.clipboardData.setData('text/plain', get_hex());
@@ -43,4 +55,11 @@ document.body.addEventListener('click', function() {
 		var audio = new Audio('hello_friend.mp3');
 		audio.play();
 	}
+});
+document.getElementById('clock').addEventListener("mouseover", function() {
+	showhex = true;
+});
+
+document.getElementById('clock').addEventListener("mouseout", function(){
+	showhex = false;
 });
